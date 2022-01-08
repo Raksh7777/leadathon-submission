e06ea52f7febfa63d3de2fc65d801b09565c3ee6
Rakshika Bawari <rakshika7777@gmail.com>
2022-01-09 03:31:00 +0530

    deployment fixes

--- services/scrape.js
+++ services/scrape.js
@@ -3,7 +3,9 @@ const puppeteer = require("puppeteer");
 exports.scrapeAllMoves = async () => {
   try {
     let chessUrl = "https://www.chessgames.com/chessecohelp.html";
-    let browser = await puppeteer.launch();
+    let browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     let page = await browser.newPage();
 
     await page.goto(
